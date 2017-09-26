7a2b2903e3cc408db710d075ea86cff2e6eafa05
lixiaoyang1996 <15755332575@163.com>
2017-09-26 15:05:54 +0800

    li

--- FirstJob/dom.js
+++ FirstJob/dom.js
@@ -1,5 +1,20 @@
-var  floatNum1 = 0.15;
-var  floatNum2 = 0.15 ;
-var sum;
-sum = floatNum1+floatNum2;
-console.log(sum);
+function cloneObiect(src) {
+    var result;
+    switch (toString.call(src)) {
+      case '[object String]':
+        result = src;
+        break;
+      case '[object Number]':
+        result = src;
+        break;
+      case '[object Array]':
+        var temp = [];
+        for (var i = 0; i < src.length; i++) {
+          // temp[i] = src[i];
+          temp[i] = cloneObiect(src[i]);
+        }
+        result = temp;
+        break;
+    }
+    return result;
+  }
